c979ab8807db23dc24bf9480639ee5df045af9e5
divyalsurse <divyal.real14@gmail.com>
2026-09-03 01:52:59 +0530

    Add User model and password hashing

---
+++ models/user.js
@@ -0,0 +1,63 @@
+const { Schema, model } = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageURL: {
+      type: String,
+      default: "/images/default.png",
+    },
+    role: {
+      type: [String],
+      enum: ["ADMIN", "USER"],
+      default: ["USER"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", function () {
+  const user = this;
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+  const hash = createHmac("sha256", salt).update(user.password).digest("hex");
+
+  this.salt = salt;
+  this.password = hash;
+});
+
+userSchema.static("matchPassword", async function (email,password) {
+  const user = await this.findOne({email});   
+  if(!user) throw new Error("User not found");
+
+  const salt = user.salt;
+  const hashedPassword = user.password
+
+  const userProvidedHash = createHmac("sha256", salt).update(password).digest("hex");
+
+    if (userProvidedHash !== hashedPassword) throw new Error("Invalid Password");
+
+  return user
+});
+
+const User = model("user", userSchema);
+module.exports = User;
